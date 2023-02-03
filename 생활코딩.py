b28a7e48856951e91ceb4a39980a02650664930e
임정빈 <steam6879@office.inha.ac.kr>
2023-02-03 13:41:49 +0900

    git commit

---
+++ 생활코딩.py
@@ -0,0 +1,9 @@
+STUDENT = [1, 2, 3, 4, 5]
+GRADES =   [2, 1, 3]
+
+print ("students[0]", STUDENT[0])
+import statistics
+print ("statistics.mean(GRADES)", statistics.mean(GRADES))
+
+import random
+print(random.choice(STUDENT))
