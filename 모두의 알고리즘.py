2cb96aa1e085ed48d9eaaad4801b19e3e0ce14c3
임정빈 <steam6879@office.inha.ac.kr>
2023-02-05 20:40:23 +0900

    git commit

--- 모두의 알고리즘.py
+++ 모두의 알고리즘.py
@@ -1,13 +1,26 @@
+# #최대값 구하기
+# def find_max(list):
+#     n = len(list)
+#     max_num = 0
+#
+#     for i in range(n):
+#         if list[i] > list[max_num] :
+#             max_num = i
+#
+#     return max_num
+#
+# a = [12, 45, 71, 15, 35, 64, 51, 28]
+# print(find_max(a))
 
-def find_max(list):
+#동명이인 찾기
+def find_same_name(a):
+    n = len(a)
+    result = set()
+    for i in range (0, n - 1):
+        for j in range (i + 1, n):
+            if a[i] == a[j]:
+                result.add(a[j])
+    return result
 
-    n = len(list)
-    max = 0
-    for i in range(n):
-        if list[i] > max :
-            max = list[i]
-
-    return max
-
-a = [112, 45, 71, 15, 35, 64, 51, 28]
-print(find_max(a))
+name = ["Tom", "Jerry", "Mike", "Jerry", "Kerry", "Kim", "Kim"]
+print(find_same_name(name))
